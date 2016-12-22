d05600236413ba16c76a5523f4430a0610858c1c
synr <oooo00owo00oooo@gmail.com>
2016-12-22 22:00:56 +0800

    Webhooks to JANDI and IFTTT

--- main.go
+++ main.go
@@ -32,6 +32,81 @@ func main() {
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 
+func HttpPost_JANDI(body, connectColor, title string) error {
+	log.Print("已經進來 JANDI POST")
+	url := "https://wh.jandi.com/connect-api/webhook/11691684/46e7f45fd4f68a021afbd844aed66430"
+	jsonStr := `{
+		"body":"` + body + `",
+		"connectColor":"` + connectColor + `",
+		"connectInfo" : [{
+				"title" : "` + title + `",
+				"description" : "這是來自 LINE BOT 的通風報信",
+				"imageUrl": "https://line.me/R/ti/p/@pyv6283b"
+		}]
+	}`
+
+	req, err := http.NewRequest(
+		"POST",
+		url,
+		bytes.NewBuffer([]byte(jsonStr)),
+	)
+	if err != nil {
+		log.Print(err)
+		return err
+	}
+
+	// Content-Type 設定
+	req.Header.Set("Accept", "application/vnd.tosslab.jandi-v2+json")
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Print(err)		
+		return err
+	}
+	defer resp.Body.Close()
+
+	log.Print(err)
+	return err
+}
+
+func HttpPost_IFTTT(body string) error {
+	//https://internal-api.ifttt.com/maker
+	log.Print("已經進來 IFTTT POST")
+	url := "https://maker.ifttt.com/trigger/linebot/with/key/WJCRNxQhGJuzPd-sUDext"
+	jsonStr := `{
+		"value1":"` + body + `",
+		"value2":"這是 LINE BOT 的同步通知",
+		"value3": "由 Heroku 的 GO 語言寫成"
+	}`
+
+	req, err := http.NewRequest(
+		"POST",
+		url,
+		bytes.NewBuffer([]byte(jsonStr)),
+	)
+	if err != nil {
+		log.Print(err)
+		return err
+	}
+
+	// Content-Type 設定
+	req.Header.Set("Accept", "application/vnd.tosslab.jandi-v2+json")
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Print(err)		
+		return err
+	}
+	defer resp.Body.Close()
+
+	log.Print(err)
+	return err
+}
+
 //MessageReceived :Callback to handle when message received.
 func MessageReceived(event Event, opts MessageOpts, msg ReceivedMessage) {
 	// log.Println("event:", event, " opt:", opts, " msg:", msg)
@@ -40,6 +115,10 @@ func MessageReceived(event Event, opts MessageOpts, msg ReceivedMessage) {
 		fmt.Println(err)
 		return
 	}
+
+	HttpPost_JANDI(opts.Sender.ID + " 從 FB 說：" + msg.Text, "yellow" , "FB 對話同步")
+	HttpPost_IFTTT(opts.Sender.ID + " 從 FB 說：" + msg.Text)
+
 	//resp, err := mess.SendSimpleMessage(opts.Sender.ID, fmt.Sprintf("Hello   , %s %s, %s", profile.FirstName, profile.LastName, msg.Text))
 	resp, err := mess.SendSimpleMessage(opts.Sender.ID, fmt.Sprintf(msg.Text))
 	log.Print("profile.FirstName = " + profile.FirstName)
