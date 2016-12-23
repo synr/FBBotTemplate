7f965a617887c6a21142c067554903bd5760b38e
synr <oooo00owo00oooo@gmail.com>
2016-12-24 04:49:46 +0800

    測試

--- main.go
+++ main.go
@@ -126,7 +126,15 @@ func MessageReceived(event Event, opts MessageOpts, msg ReceivedMessage) {
 	log.Print("profile.FirstName = " + profile.FirstName)
 	log.Print("profile.LastName = " + profile.LastName)
 	log.Print("opts.Sender.ID = " + opts.Sender.ID)
+	//https://developers.facebook.com/docs/messenger-platform/webhook-reference/message
 	log.Print("msg.Text = " + msg.Text)
+	log.Print("msg.mid = " + msg.mid)
+	log.Print("msg.seq = " + msg.seq)
+	log.Print("msg.attachment = " + msg.attachment)
+	log.Print("msg.quick_reply = " + msg.quick_reply)
+
+	log.Print("location.coordinates.lat = " + location.coordinates.lat)
+	log.Print("location.coordinates.long = " + location.coordinates.long)
 
 	if err != nil {
 		fmt.Println(err)
